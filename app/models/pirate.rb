3c176e3456d6bd084e4a16598f3306bc6def123b
TheNanoBay <noorani.mehdi@outlook.com>
2021-01-14 10:41:46 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+    @@pirates = [] 
+
+    attr_reader :name, :height, :weight
+
+    def initialize(args) #--> assumes args is in the form of a hash 
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
-end
+    attr_reader :name, :type, :booty
+  
+    @@ships = []
+  
+    def initialize(args) #--> assumes args is in the form of a hash
+      @name = args[:name]
+      @type = args[:type]
+      @booty = args[:booty]
+      @@ships << self
+    end
+  
+    def self.all
+      @@ships
+    end
+  
+    def self.clear
+      @@ships = []
+    end
+  end
